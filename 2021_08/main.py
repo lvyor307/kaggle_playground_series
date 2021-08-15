fb04a18fbf1745b10837eecb190cb8db1241a590
Or Levi <lvyor307@gmail.com>
2021-08-15 23:04:23 +0300

    finish

--- 2021_08/main.py
+++ 2021_08/main.py
@@ -28,22 +28,19 @@ if os.path.isfile('test.csv') and os.path.isfile(filename + '.zip'):
 Read data and split to train test
 """
 
-df: pd.DataFrame = pd.read_csv('train.csv')  # read data
+df_train: pd.DataFrame = pd.read_csv('train.csv')  # read data
 
 # Split to features (X) and lable (y)
-data: np.ndarray = df.values
-X: np.ndarray = data[:, 1:-1]
-y: np.ndarray = data[:, -1]
+data_train: np.ndarray = df_train.values
+X_train: np.ndarray = data_train[:, 1:-1]
+y_train: np.ndarray = data_train[:, -1]
 
-# Split data to train and test
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.8, random_state=217)
-
-""""""
 
 """
 Random Forest Model
 """
+# Number of trees
+n_estimators = [100, 200, 300, 1000]
 # Number of features to consider at every split
 max_features = [int(x) for x in np.linspace(start=1, stop=99, num=2)]
 # Minimum number of samples required to split a node
@@ -52,24 +49,57 @@ min_samples_split = [2, 5, 10]
 min_samples_leaf = [1, 2, 4]
 # Method of selecting samples for training each tree
 bootstrap = [True, False]
-# Create the random grid
-random_grid = {'max_features': max_features,
-               'min_samples_split': min_samples_split,
-               'min_samples_leaf': min_samples_leaf,
-               'bootstrap': bootstrap}
+# Create the grid
+grid = {'max_features': max_features,
+        'min_samples_split': min_samples_split,
+        'min_samples_leaf': min_samples_leaf,
+        'n_estimators': n_estimators,
+        'bootstrap': bootstrap}
 
 # define model
 rf = RandomForestRegressor()
 # define model evaluation method
 cv = RepeatedKFold(n_splits=5, n_repeats=1, random_state=217)
 # define search
-search = GridSearchCV(estimator=rf,
-                      param_grid=random_grid,
-                      scoring='neg_root_mean_squared_error',
-                      cv=cv,
-                      n_jobs=-1)
+grid_search = GridSearchCV(estimator=rf,
+                           param_grid=grid,
+                           scoring='neg_root_mean_squared_error',
+                           cv=cv,
+                           n_jobs=-1)
 # perform the search
-results = search.fit(X_train, y_train)
+results = grid_search.fit(X_train, y_train)
 
-a = 1
-print('a')
+
+"""
+ Get the paramters that gives the best RMSE
+"""
+
+best_grid = grid_search.best_estimator_
+
+rf_cv = RandomForestRegressor(max_features=best_grid.get('max_features'),
+                              min_samples_split=best_grid.get(
+                                  'min_samples_split'),
+                              min_samples_leaf=best_grid.get(
+                                  'min_samples_leaf'),
+                              n_estimators=best_grid.get('n_estimators'),
+                              bootstrap=best_grid.get('bootstrap'))
+
+rf_cv.fit(X_train, y_train)
+
+
+"""
+Predictions to test set
+"""
+df_test: pd.DataFrame = pd.read_csv('test.csv')  # read data
+
+data_test: np.ndarray = df_test.values
+X_test: np.ndarray = data_test[:, 1:-1]
+
+loss = rf_cv.predict(X_test)
+
+prediction: pd.DataFrame = pd.DataFrame({
+    'id': df_test.values[:, 0].astype(int),
+    'loss': loss
+})
+
+prediction.to_csv('prediction.csv', index=False)
